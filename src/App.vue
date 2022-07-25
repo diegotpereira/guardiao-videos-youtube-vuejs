a6a515cb37ff3247cdf08b75c7d8bd559997c4d5
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-07-25 11:20:58 -0300

    criação parcial dos arquivos de configuração.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
